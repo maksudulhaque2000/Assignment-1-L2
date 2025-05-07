a06773fc084f770bc00bb66f952f806fd2be8136
Maksudul-Haque <smmaksudulhaque2000@gmail.com>
2025-05-07 22:38:52 +0600

    Solved Problem-2

--- assignment-1.ts
+++ assignment-1.ts
@@ -2,3 +2,8 @@
 function formatString(input: string, toUpper?: boolean): string {
     return (toUpper ?? true) ? input.toUpperCase() : input.toLowerCase();
   }
+
+// Problem 2
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4);
+  }
